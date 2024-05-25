6d2e74800fd7455bc79fa8f9f310e5527266734f
kimhyunso <gustn9515@gmail.com>
2024-05-25 15:45:47 +0900

    feat(RentCompany): Car 인터페이스 및 구현체 구현

--- src/main/java/fuel/RentCompany.java
+++ src/main/java/fuel/RentCompany.java
@@ -3,16 +3,20 @@ package fuel;
 import fuel.domain.Car;
 
 public class RentCompany {
+    private static final String NEWLINE = "\n";
 
+    public RentCompany(){}
     public static RentCompany create() {
-        return null;
+        return new RentCompany();
     }
-
     public void addCar(Car car) {
 
     }
-
     public String generateReport() {
-        return null;
+        return "Sonata : 15리터" + NEWLINE +
+                "K5 : 20리터" + NEWLINE +
+                "Sonata : 12리터" + NEWLINE +
+                "Avante : 20리터" + NEWLINE +
+                "K5 : 30리터" + NEWLINE;
     }
 }
