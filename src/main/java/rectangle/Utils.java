6cd801bf16b12cc4f74dece430d430b87bd1d56b
kimhyunso <gustn9515@gmail.com>
2024-06-04 15:07:06 +0900

    feat: 입력 문자 중 특수문자를 제거하는 기능 구현

---
+++ src/main/java/rectangle/Utils.java
@@ -0,0 +1,24 @@
+package rectangle;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Utils {
+
+    public static final String REGEX = "[(|,|)|-]";
+
+    public static List<Integer> split(String input) {
+        return Arrays.stream(input.split(REGEX))
+                .filter(Utils::isNotEmpty)
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isNotEmpty(String input){
+        return !input.isEmpty();
+    }
+}
---
+++ src/test/java/rectangle/UtilsTest.java
@@ -0,0 +1,20 @@
+package rectangle;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class UtilsTest {
+
+    @Test
+    @DisplayName("입력 문자 중 특수문자를 제거 후 하는 기능")
+    void 특수문자제거(){
+        assertThat(Utils.split("(10,10)-(22,10)-(22,18)-(10,18)")).isEqualTo(Arrays.asList(10, 10, 22, 10, 22, 18, 10, 18));
+    }
+
+
+}
